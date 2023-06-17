61d2bd54cf9b0256412a7a6512eebc949c4998ad
BrianFonseca <brianfonseca1@outlook.com>
2023-06-17 19:57:17 -0300

    Commit inicial del proyecto

---
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet] 
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() 
+            { 
+                new Autor() { Id = 1, Nombre = "Brian"},
+                new Autor() { Id = 2, Nombre = "Claudia"}
+            };
+        }
+    }
+}
